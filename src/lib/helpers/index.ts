d0005c22b1f83f2c00ad2634c3997bd2ded7bb34
Kevin Bervoets <kevin.bervoets@proton.me>
2025-02-05 21:59:40 +0100

    make tailwindconfig available in typescript

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -1,6 +1,10 @@
+import tailwindConfig from "../../../tailwind.config";
 import { ClassValue, clsx } from "clsx";
+import resolveConfig from "tailwindcss/resolveConfig";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const getTailwindConfig = () => resolveConfig(tailwindConfig);
