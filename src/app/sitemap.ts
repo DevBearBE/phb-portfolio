6295cbc77f2d68c23c93e6ded768d346c3cb2d5f
Kevin Bervoets <kevin.bervoets@proton.me>
2025-03-11 23:11:39 +0100

    add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://praktijkhetbergsken.be/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://praktijkhetbergsken.be/over',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://praktijkhetbergsken.be/aanbod/diagnostisch-onderzoek',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://praktijkhetbergsken.be/aanbod/begeleiding',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://praktijkhetbergsken.be/team',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://praktijkhetbergsken.be/contact',
+            lastModified: new Date(),
+        },
+    ];
+}
